7d44e4d1db6ae41b569f75877a8e3bab735a7a4e
RuslanSyrymov <prototyp200020@yandex.ru>
2025-04-12 02:21:37 +0700

    0

--- frontend/spa/public/src/plugins/configsys.js
+++
@@ -1,7 +0,0 @@
-import configsys from '../config/index.js'
-
-export default {
-    install: (app) => {
-        app.config.globalProperties.$configsys = configsys;
-    }
-}
--- frontend/spa/public/src/plugins/network.js
+++
@@ -1,7 +0,0 @@
-import network from '../api/index.js'
-
-export default {
-    install: (app) => {
-        app.config.globalProperties.$network = network;
-    }
-}
